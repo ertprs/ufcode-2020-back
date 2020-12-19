baecbdc70ddfa19d10137d4d1e7e2e2bedf15970
Augusto Lessa <augustolessa_c@hotmail.com>
2020-12-19 19:36:27 -0300

    adds loan-request model

---
+++ src/models/loan-request.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const loanRequestSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    value: {
+        type: Number,
+        required: true,
+    },
+    numInstallments: {
+        type: Number,
+        required: true,
+    },
+    valueInstallments: {
+        type: Number,
+        required: true,
+    },
+    link: {
+        type: String,
+        default: '',
+    },
+    status: {
+        /**
+         * Possible values:
+         * - in_progress
+         * - accepted
+         * - denied
+         */
+        type: String,
+        default: 'in_progress'
+    },
+}, {
+    timestamps: true
+})
+
+const LoanRequest = mongoose.model('LoanRequest', loanRequestSchema)
+
+module.exports = LoanRequest
