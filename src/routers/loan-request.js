fdc296af5f5ff131b4fb9d86f225554e063e7419
Augusto Lessa <augustolessa_c@hotmail.com>
2020-12-20 14:10:15 -0300

    corrects problem with wpp bot sending message only to the exact number

--- src/routers/loan-request.js
+++ src/routers/loan-request.js
@@ -26,12 +26,33 @@ router.post('/loan-requests', async (req, res) => {
         await loanRequest.save()
 
         /**
+         * If the user's whatsapp number does not have an extra nine, we'll have some problems here: passing
+         * - 319XXXXXXXX
+         * - 31XXXXXXXX
+         * is not the same thing to our bot! It'll only send a message if the user's wpp number matches perfectly with what we'll
+         * pass in the post request below. Because of that, we'll make one request to the number with an extra digit and one
+         * to the number without the extra digit (they'll always belong to the same person, of course)
+         */
+        let extraDigitNumber = user.phone.length === 11 ? user.phone : `${[user.phone.slice(0,3),'9',user.phone.slice(3)].join('')}`
+        let oldNumberStyle = user.phone.length === 10 ? user.phone : `${user.phone.slice(0,3)}${user.phone.slice(4)}`
+
+        /**
          * In the real world, this probably wouldn't work because the external API may not have a webhook (and that's exactly
          * what we're simulating here). We would have to create a polling.
          */
         await axios.post(FAKE_WEBHOOK_URL, {
             'from': '',
-            'to': user.phone,
+            'to': extraDigitNumber,
+            'contents': [
+                {
+                'type': 'text',
+                'text': `Olá, *${user.name}*!\n\nRecebemos sua solicitação de empréstimo 💰\n\nSeu pedido de número *${loanRequest._id.toString().slice(0,10)}* está sendo analisado, em breve retornaremos com notícias sobre seu status.\n\nObrigado por comprar com a Empresta!`
+                }
+            ]
+        })
+        await axios.post(FAKE_WEBHOOK_URL, {
+            'from': '',
+            'to': oldNumberStyle,
             'contents': [
                 {
                 'type': 'text',
